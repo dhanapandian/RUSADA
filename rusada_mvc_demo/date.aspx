f70c063e0ccbfcc027ebd35c90d607170f1d3b29
dhanapandian <42737877+dhanapandian@users.noreply.github.com>
2022-09-15 11:26:55 +0530

    Add files via upload

---
+++ rusada_mvc_demo/date.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="date.aspx.cs" Inherits="Rusada_Mvc_Demo.date" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+
+
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lbldate" runat="server"></asp:Label>
+            <br />
+            <br />
+          <asp:button runat="server" text="Button" ID="btn_insert" OnClick="Unnamed1_Click" /> 
+        </div>
+    </form>
+</body>
+</html>
---
+++ rusada_mvc_demo/date.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Rusada_Mvc_Demo
+{
+    public partial class date : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            DateTime thisDate1 = new DateTime(2011, 6, 10);
+            Console.WriteLine("Today is " + thisDate1.ToString("MMMM dd, yyyy") + ".");
+
+            DateTimeOffset thisDate2 = new DateTimeOffset(2011, 6, 10, 15, 24, 16,
+                                                          TimeSpan.Zero);
+            Console.WriteLine("The current date and time: {0:MM/dd/yy H:mm:ss zzz}",
+                               thisDate2);
+        }
+    }
+}
